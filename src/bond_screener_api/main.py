bd59c572f10d73c9b79ada13bd0c3ea5e5404358
Aleksandr Krivosheev <77s.a.n.e.k.77@gmail.com>
2024-03-11 22:39:27 +0600

    Create table view

--- src/bond_screener_api/main.py
+++ src/bond_screener_api/main.py
@@ -1,5 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from schemas.bond import ColumnGroupModel
 import uvicorn
 
 # from api import router as api_router
@@ -7,6 +10,7 @@ from dependencies import fastapi_service
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 # app.include_router(api_router.router)
 
@@ -20,6 +24,19 @@ async def root():
 async def get_bonds(
         fastapi_service: Annotated[dict, Depends(fastapi_service)]):
     return fastapi_service
+
+
+@app.get('/view_bonds', response_class=HTMLResponse)
+async def get_view_bonds(
+        request: Request,
+        fastapi_service: Annotated[ColumnGroupModel,
+                                   Depends(fastapi_service)]):
+    columns = fastapi_service.columns
+    data = fastapi_service.data
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+        context={'columns': columns, 'data': data})
 
 
 if __name__ == '__main__':
